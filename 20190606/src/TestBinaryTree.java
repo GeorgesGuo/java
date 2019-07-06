6af7f0ee082be162db53aff3aba69fc5cb1804f3
GeorgesGuo <18509279216@163.com>
2019-07-06 16:14:49 +0800

    数据结构

---
+++ 20190606/src/TestBinaryTree.java
@@ -0,0 +1,12 @@
+import static jdk.nashorn.internal.objects.Global.print;
+
+public class TestBinaryTree {
+    public static void main(String[] args) {
+        int x = 20;
+        int y = 13;
+       print("%d\n",3*(y/2));
+
+
+
+    }
+}
