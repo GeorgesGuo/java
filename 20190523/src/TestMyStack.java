68464ddf145d1f52024d5ab1da719196e08be22d
GorgeousGuo <18509279216@163.com>
2019-10-07 21:49:47 +0800

    2019

--- 20190523/src/TestMyStack.java
+++ 20190523/src/TestMyStack.java
@@ -1,20 +1,16 @@
 /**
  * Created with IntelliJ IDEA.
  * Description: 两个队列实现栈
- * User: GAO BO
- * Date: 2019-05-23
- * Time: 21:02
+
  */
 public class TestMyStack {
     private MyQueueImp1 myQueue1;
     private MyQueueImp1 myQueue2;
     private int usedSize;
-
     public TestMyStack() {
         myQueue1 = new MyQueueImp1();
         myQueue2 = new MyQueueImp1();
     }
-
     /**
      * Push element x onto stack.
      */
@@ -23,13 +19,11 @@ public class TestMyStack {
             myQueue1.add(x);
         } else if ( !myQueue2.empty() ) {
             myQueue2.add(x);
-
         } else {
             myQueue1.add(x);
         }
 
     }
-
     /**
      * Removes the element on top of the stack and returns that element.
      */
@@ -49,8 +43,6 @@ public class TestMyStack {
         return data;
 
     }
-
-
     /**
      * Get the top element.
      */
@@ -72,8 +64,6 @@ public class TestMyStack {
         return data;
 
     }
-
-
     /**
      * Returns whether the stack is empty.
      */
